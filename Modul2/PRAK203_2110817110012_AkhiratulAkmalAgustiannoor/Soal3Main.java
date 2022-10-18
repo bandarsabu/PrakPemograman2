4e07b0d657e75a15207f9343b805e19b04d00899
Akhiratul Akmal Agustiannoor <102504413+bandarsabu@users.noreply.github.com>
2022-10-18 23:36:15 +0700

    Update Soal3Main.java

--- Modul2/PRAK203_2110817110012_AkhiratulAkmalAgustiannoor/Soal3Main.java
+++ Modul2/PRAK203_2110817110012_AkhiratulAkmalAgustiannoor/Soal3Main.java
@@ -3,14 +3,22 @@ package PRAK203_2110817110012_AkhiratulAkmalAgustiannoor;
 public class Soal3Main {
     public static void main(String[] args){
         Pegawai p1 = new Pegawai();
+    //  Pada baris ini terjadi error karena kurangnya titik koma (;)
+    //  p1.nama = "Roi"
         p1.nama = "Roi";
         p1.asal = "Kingdom of Orvel";
         p1.setJabatan("Assasin");
+
+//      Karena umur masih belum bervalue maka kita beri value pada umur
         p1.umur = 17;
-        
+
+//      Pada baris ini tidak sesuai dengan hasil output maka, kita hapus "Pegawai"
+//      System.out.println("Nama Pegawai: " + p1.getNama());
         System.out.println("Nama: " + p1.getNama());
         System.out.println("Asal: " + p1.getAsal());
         System.out.println("Jabatan: " + p1.jabatan);
+//      Pada baris berikut masih kurang lengkap yaitu mencetak "tahun" pada akhir
+//      System.out.println("Umur: " + p1.umur);
         System.out.println("Umur: " + p1.umur + " tahun");
     }
 }
