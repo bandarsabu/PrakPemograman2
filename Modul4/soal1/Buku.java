91713358b563764c6b3dc9f194ed3d18a23d6133
Akhiratul Akmal Agustiannoor <102504413+bandarsabu@users.noreply.github.com>
2022-11-15 23:02:42 +0700

    Update Buku.java

--- Modul4/soal1/Buku.java
+++ Modul4/soal1/Buku.java
@@ -1,19 +1,20 @@
 package praktikum4.soal1;
 
 public class Buku {
-    private String judul, penulis;
-    private int tahun;
+    private String judul;
+    private String penulis;
+    private Integer tahun;
 
-    public Buku(String j, String p, int t) {
+    public Buku(String j, String p, int t){
         this.judul = j;
         this.penulis = p;
         this.tahun = t;
     }
-
-    public void display(){
-        System.out.println("Detail Buku :");
-        System.out.println("Judul adalah " + judul);
-        System.out.println("penulis adalah" + penulis);
-        System.out.println("Tahun Terbit adalah " + tahun);
-    }
+    
+    void display(){
+        System.out.println("Detail buku:");
+        System.out.println("Judul adalah " + this.judul);
+        System.out.println("Penulis adalah " + this.penulis);
+        System.out.println("Tahun Terbit adalah " + this.tahun);
+    }   
 }
