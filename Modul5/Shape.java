c7228bb539b23c8c9db78611bda79f68e0c3042a
Akhiratul Akmal Agustiannoor <102504413+bandarsabu@users.noreply.github.com>
2022-11-29 23:41:33 +0700

    Update Shape.java

--- Modul5/Shape.java
+++ Modul5/Shape.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package praktikum5.soal1;
 
 public abstract class Shape {
